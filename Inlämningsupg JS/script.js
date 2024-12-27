fad0478c7c8a5173f0e03297ee18471902d6df51
sandratur <sandra.turesson.webd24jon@edu.tucsweden.se>
2024-12-27 13:20:21 +0100

    Första uppladdning Inlämningsupg JS1

---
+++ Inlämningsupg JS/script.js
@@ -0,0 +1,76 @@
+
+const card1 = document.getElementById("card1");
+const photo1 = document.querySelector("#photo1");
+const backImg = document.getElementById("backImg1");
+
+function turnCard() {
+    console.log("du klicka på kort 1");
+    
+    photo1.style.display="flex";
+    photo1.style.justifyContent = "center";
+    photo1.style.alignItems = "center";
+      
+}
+card1.addEventListener("click", turnCard);
+
+
+
+const card2 = document.getElementById("card2");
+const photo2 = document.querySelector("#photo2");
+
+
+function turnCard2() {
+    console.log("du klicka på kort 2");
+    
+    photo2.style.display="flex";
+    photo2.style.justifyContent = "center";
+    photo2.style.alignItems = "center";
+}
+card2.addEventListener("click", turnCard2);
+
+
+
+const card3 = document.getElementById("card3");
+const photo3 = document.querySelector("#photo3");
+
+
+function turnCard3() {
+    console.log("du klicka på kort 3");
+    
+    photo3.style.display="flex";
+    photo3.style.justifyContent = "center";
+    photo3.style.alignItems = "center";
+}
+card3.addEventListener("click", turnCard3);
+
+
+
+const card4 = document.getElementById("card4");
+const photo4 = document.querySelector("#photo4");
+
+
+function turnCard4() {
+    console.log("du klicka på kort 4");
+    
+    photo4.style.display="flex";
+    photo4.style.justifyContent = "center";
+    photo4.style.alignItems = "center";
+    
+}
+card4.addEventListener("click", turnCard4);
+
+
+
+const card5 = document.getElementById("card5");
+const photo5 = document.querySelector("#photo5");
+
+
+function turnCard5() {
+    console.log("du klicka på kort 5");
+    
+    photo5.style.display="flex";
+    photo5.style.justifyContent = "center";
+    photo5.style.alignItems = "center";
+    
+}
+card5.addEventListener("click", turnCard5);
